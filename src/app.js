d31d4b91e4ccb3eda149bd95264a63328b639de1
Abhijay-Prakash <abhijayprakash18@gmail.com>
2025-05-09 23:02:40 +0530

    cors new origin added

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: ["http://localhost:5173",
+      origin: ["http://localhost:5173","https://fhc-admin-panel.vercel.app",
       "http://localhost:5174"],
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
