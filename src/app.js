fd7a30f10ad061ba5df9dc503ec96cc5d3747dce
Abhijay-Prakash <148245052+Abhijay-Prakash@users.noreply.github.com>
2025-05-13 23:57:01 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ const app = express();
 
 app.use(
     cors({
-      origin: ["http://localhost:5173", "http://localhost:5174","https://fhc-admin-panel.vercel.app","https://fhc-portal-frontend-9uxo.vercel.app","http://localhost:3000"],
+      origin: ["http://localhost:5173", "http://localhost:5174","https://fhc-admin-panel.vercel.app","https://fhc-portal-frontend-9uxo.vercel.app","http://localhost:3000","https://fhc-portal-fe.vercel.app"],
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
       allowedHeaders: ["Content-Type", "Authorization"],
