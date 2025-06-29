1b0559f86a7bea8c7a1db276300a49ebe6cd51fb
Abhijay-Prakash <abhijayprakash18@gmail.com>
2025-06-29 19:51:54 +0530

    updated event controller as per new schema

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -16,15 +16,14 @@ export const getAllEvents = async (req, res) => {
 
 export const registerForEvent = async (req, res) => {
   const { eventId } = req.body;
-
-  const userId = req.user._id; 
-  const user = await User.findById(userId);
+  const userId = req.user._id;
 
   if (!eventId) {
     return res.status(400).json({ message: 'Event ID is required' });
   }
 
   try {
+    const user = await User.findById(userId);
     const event = await Event.findById(eventId);
 
     if (!event) {
@@ -32,29 +31,45 @@ export const registerForEvent = async (req, res) => {
     }
 
     
+    if (event.registrationStatus !== 'Open') {
+      return res.status(403).json({
+        message: `Registration is currently ${event.registrationStatus.toLowerCase()} for this event`
+      });
+    }
+
+    
     if (event.registeredUsers.includes(userId)) {
       return res.status(400).json({ message: 'User is already registered for this event' });
     }
 
-    
+   
     if (event.attendees >= event.capacity) {
+      
+      if (event.registrationStatus !== 'Full') {
+        event.registrationStatus = 'Full';
+        await event.save();
+      }
       return res.status(400).json({ message: 'Event is full' });
     }
 
+
     event.registeredUsers.push(userId);
     event.attendees += 1;
+
+    
+    if (event.attendees >= event.capacity) {
+      event.registrationStatus = 'Full';
+    }
+
     await event.save();
 
-   
     await User.findByIdAndUpdate(userId, {
       $addToSet: { eventsRegistered: eventId }
-    }, { new: true });
+    });
 
-    //await sendRegistrationMail(user.email,user.name,event.title);
-
+    
     if (event.category === 'Meeting') {
       const meetingLink = 'https://meet.google.com/ajs-nkqq-bdw'; 
-
       await sendMeetingMail({
         to: user.email,
         name: user.name,
@@ -65,15 +80,13 @@ export const registerForEvent = async (req, res) => {
       });
     }
 
-
-
-
-
     res.status(200).json({ message: 'Registered for event successfully' });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    console.error("Error in event registration:", err.message);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
+
 
 
 
@@ -89,6 +102,9 @@ export const getRegisteredEvents = async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+
+
 
 
 //admin features below this
