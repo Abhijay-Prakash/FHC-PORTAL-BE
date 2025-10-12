fad2a59963aa4de04abe55f852817ac17a60daec
Abhijay-Prakash <abhijayprakash18@gmail.com>
2025-10-12 22:07:17 +0530

    'approve-collab-routes'

--- src/routes/project/userProjectRoutes.js
+++ src/routes/project/userProjectRoutes.js
@@ -4,6 +4,8 @@ import {
   getApprovedProjects,
   requestToJoin,
   getMyProjects,
+  getJoinRequests,
+  respondToJoinRequest
 } from "../../controllers/project/userProjectController.js";
 import verifyUser from "../../middleware/protectRoute.js";
 
@@ -14,5 +16,10 @@ router.post("/propose", verifyUser, proposeProject);
 router.get("/approved", getApprovedProjects);
 router.post("/join", verifyUser, requestToJoin);
 router.get("/my-projects", verifyUser, getMyProjects);
+
+router.get("/:projectId/requests", verifyUser, getJoinRequests);
+
+router.put("/:projectId/requests/:requestId/respond", verifyUser, respondToJoinRequest);
+
 
 export default router;
