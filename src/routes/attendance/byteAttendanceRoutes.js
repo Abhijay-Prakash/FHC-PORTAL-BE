48f7e940355504fd227d9131304b9b316c22364e
Abhijay-Prakash <abhijayprakash18@gmail.com>
2025-11-22 16:31:31 +0530

    'export-type-fix'

--- src/routes/attendance/byteAttendanceRoutes.js
+++ src/routes/attendance/byteAttendanceRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { markAttendance,getAttendanceByDate } from "../../controllers/attendance/byteAttendanceController.js";
-import protectRoute from "../../middleware/protectRoute.js";
+import {protectRoute} from "../../middleware/protectRoute.js";
 const router = express.Router()
 router.post('/mark-attendance',protectRoute, markAttendance);
 router.get('/attendance',protectRoute,getAttendanceByDate);
