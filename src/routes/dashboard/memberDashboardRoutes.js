813fc733f760930dffed03fb7b45ca5122b7ad8e
Abhijay-Prakash <abhijayprakash18@gmail.com>
2025-11-22 16:30:37 +0530

    'export-type-fix'

--- src/routes/dashboard/memberDashboardRoutes.js
+++ src/routes/dashboard/memberDashboardRoutes.js
@@ -4,7 +4,7 @@ import {
   getUpcomingUserEvents
 } from '../../controllers/dashboard/memberDashboardController.js';
 
-import protectRoute from '../../middleware/protectRoute.js';
+import {protectRoute} from '../../middleware/protectRoute.js';
 
 const router = express.Router();
 
