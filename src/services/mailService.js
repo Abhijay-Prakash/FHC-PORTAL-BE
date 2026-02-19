f3bbc323840b717b07cc2724c09608b2ffee83eb
Abhijay-Prakash <abhijayprakash18@gmail.com>
2026-02-19 16:38:38 +0530

    mail-fix

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -5,11 +5,16 @@ dotenv.config();
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 15000,
+  greetingTimeout: 10000,
+  socketTimeout: 15000,
 });
 
 
